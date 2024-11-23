3bbcbf721227e61a6ee9cdb69996b10f6e6ea693
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-23 13:30:01 +0300

    Update logger.py

--- WinxMusic/utils/logger.py
+++ WinxMusic/utils/logger.py
@@ -1,38 +1,61 @@
-from pyrogram.types import Message
-
+from config import LOG, LOG_GROUP_ID
+import psutil
+import time
 from WinxMusic import app
 from WinxMusic.utils.database import is_on_off
-from config import LOG, LOG_GROUP_ID
+from WinxMusic.utils.database.memorydatabase import (
+    get_active_chats, get_active_video_chats)
+from WinxMusic.utils.database import (get_global_tops,
+                                       get_particulars, get_queries,
+                                       get_served_chats,
+                                       get_served_users, get_sudoers,
+                                       get_top_chats, get_topp_users)
 
 
-async def play_logs(message: Message, streamtype: str):
+
+async def play_logs(message, streamtype):
+    chat_id = message.chat.id
+    sayı = await app.get_chat_members_count(chat_id)
+    toplamgrup = len(await get_served_chats())
+    aktifseslisayısı = len(await get_active_chats())
+    aktifvideosayısı = len(await get_active_video_chats())
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    CPU = f"{cpu}%"
+    RAM = f"{mem}%"
+    DISK = f"{disk}%"
+
+
     if await is_on_off(LOG):
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
-            chatusername = "🔒 Grupo Privado"
-
+            chatusername = "Gizli Grup 🔏"
         logger_text = f"""
-🎵 **Registro de Reprodução - {app.mention}** 🎵
 
-📌 **ID do Chat:** `{message.chat.id}`
-🏷️ **Nome do Chat:** {message.chat.title}
-🔗 **Nome de Usuário do Chat:** {chatusername}
 
-👤 **ID do Usuário:** `{message.from_user.id}`
-📛 **Nome:** {message.from_user.mention}
-📱 **Nome de Usuário:** @{message.from_user.username}
+🍭 **Grup Adı:** {message.chat.title} [`{message.chat.id}`]
+👥 **Üye Sayısı: {sayı}**
+👤 **Kullanıcı:** {message.from_user.mention}
+✏️ **Kullanıcı Adı:** @{message.from_user.username}
+🆔 **Kullanıcı ID:** `{message.from_user.id}`
+🔗 **Grup Linki:** {chatusername}
+🔎 **Sorgu:** {message.text}
 
-🔍 **Consulta:** {message.text.split(None, 1)[1]}
-🎧 **Tipo de Transmissão:** {streamtype}"""
+**İşlemci:** {CPU}  💢  **Bellek:** {RAM}  📂  **Depolama:** {DISK}
 
+**Toplam Grup Sayısı: 👉 {toplamgrup}** 
+
+**Aktif Ses: {aktifseslisayısı}  🌬️  Aktif Video: {aktifvideosayısı}**"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    chat_id=LOG_GROUP_ID,
-                    text=logger_text,
+                    LOG_GROUP_ID,
+                    f"{logger_text}",
                     disable_web_page_preview=True,
                 )
-            except Exception as e:
-                print(e)
+                await app.set_chat_title(LOG_GROUP_ID, f"AKTİF SESLİ - {aktifseslisayısı}")
+            except:
+                pass
         return
